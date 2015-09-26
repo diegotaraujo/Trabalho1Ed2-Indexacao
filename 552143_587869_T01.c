3e054fc1e237b8899c0f1eb8e14ac81eabd06cda
Diego Tenorio de Araujo Siqueira <diegotaraujo@gmail.com>
2015-09-26 20:00:17 -0300

    Criação dos prototipos e inserirPartida

--- 552143_587869_T01.c
+++ 552143_587869_T01.c
@@ -27,6 +27,7 @@ Autores:
 //setar o tamanho das strings!<<<<<<<<<<<<<< um byte a mais que especificado no pdf para guardar o '\0' (6 bytes serão guardados como 5 no 
 //arquivo quando excluirmos o '\0')
 
+//Definição das estruturas usadas
 struct data {
 	int dia[3];
 	int mes[3];
@@ -46,6 +47,22 @@ struct partida {
 	char chave_primaria[9];
 };
 typedef struct partida Partida;
+
+//Prototipos das funções usadas
+					//Estrutura de dados para armazenar os indices em RAM. ??????????????NÃO ENTENDI??????????
+verificarArquivoDados(); //Verificar se o arquivo de dados existe
+verificarIndicePrimario(); //Verificar se o indice primario existe
+verificarIndiceSecundario(); //Verificar se os indices secundários existem
+criarIndicePrimario(); //Criar o indice primário
+criarIndiceSecundario(); //Criar os indices secundários
+carregarIndices(); //Carregar os indices (DISCO PARA RAM)
+Partida inserirPartida(); //Inserir registro
+buscarPartida(); //Buscar registro
+alterarPartida(); //Alterar registro
+removerPartida(); //Remover registro
+listarPartidas(); //Listar registros
+liberarEspaço(); //Liberar espaço
+atualizarIndices(); //Atualizar todos os indices
 
 int main () {
 	/*variaveis para arquivos, respectivamente: de dados, de indice primario, 
@@ -72,9 +89,12 @@ int main () {
 
 		switch(opc) {
 			case INSERIR_PARTIDA:
-				Partida novo;
 
-				novo.equipe_azul = le_nome_equipe();
+				Partida p;
+
+				p = inserirPartida(); // unica funcao para leitura dos campos. Devolve uma struct partida
+				
+				/*novo.equipe_azul = le_nome_equipe();
 				novo.equipe_vermelha = le_nome_equipe();
 				data_partida = le_data();
 				novo.duracao = le_duracao();
@@ -83,12 +103,29 @@ int main () {
 				novo.placar_vermelha = le_placar();
 				apelido_mvp = le_apelido_mvp();
 				//GERAR CHAVE PRIMARIA, FUNÇÃO JÁ DECLARADA
+				*/
 			break;
 
 			case REMOVER_PARTIDA:
+			
+				if(buscarPartida())
+					//se encontrar devolve a posicao
+					removerPartida()
+				
+				else
+					printf("Partida não encontrada!\n");
+				
 			break;
 
 			case MODIFICAR_DURACAO:
+			
+				if(buscarPartida())
+					//se encontrar devolve a posicao
+					alterarPartida()
+				
+				else
+					printf("Partida não encontrada!\n");
+				
 			break;
 
 			case BUSCAR_PARTIDA:
@@ -119,6 +156,152 @@ int main () {
 }
 
 /*==================- IMPLEMENTAÇÃO DAS FUNÇÕES -==================*/
+
+Partida inserirPartida(){
+	
+	Partida novo;
+	
+	int flag = 0;
+
+	char equipeAzul[40], equipeVermelha[40], equipeVencedora[40], apelidoMvp[40], duracaoPartida[6], placarAzul[3], placarVermelha[3], chavePK[9];
+	Data dataPartida;
+	
+	//Leitura das equipes
+	scanf("%39[\n]", equipeAzul);
+	getchar();
+	
+	scanf("%39[\n]", equipeVermelha);
+	getchar();
+	
+	
+	novo.equipe_azul = equipeAzul;
+	novo.equipe_vermelha = equipeVermelha;
+		
+	//Leitura da data
+	
+	flag = 0;
+	do {
+		flag = 0;
+		scanf("%d/%d/%d", &dataPartida.dia, &dataPartida.mes, &dataPartida.ano);
+		if(dataPartida.dia < 1 || dataPartida.dia > 31 || dataPartida.mes < 1 || dataPartida.mes > 12 || dataPartida.ano < 2011 || dataPartida.ano > 2015) {
+			flag = 1;
+			printf(ENTRADA_INVALIDA);
+		}
+	} while(flag == 1);
+
+	novo.data_partida = dataPartida;
+
+	//Leitura da duracao
+	
+	flag = 0;
+	do {
+		scanf("%5[\n]", duracaoPartida);
+		getchar();	//Tira \n do buffer
+		flag = strlen(duracaoPartida);
+		if(flag != 5)
+			printf(ENTRADA_INVALIDA);
+	} while (flag != 5);
+
+	novo.duracao = duracaoPartida;
+
+	//Leitura da equipe vencedora
+	
+	flag = 0;
+	do{
+		scanf("%39[\n]", equipeVencedora);
+		getchar(); //Tira \n do buffer
+		
+		if(!strcmp(equipeAzul, equipeVencedora)
+			flag = 1;
+		else if(!strcmp(equipeVermelha, equipeVencedora)
+			flag = 1;
+		else
+			flag = 0;
+
+	}while(flag == 0);
+	
+	novo.equipe_vencedora = equipeVencedora;
+
+	//Leitura dos placares
+	
+	flag = 0;
+
+	do {
+		scanf("%2[\n]", placarAzul);
+		getchar();	//Tira \n do buffer
+		flag = strlen(placarAzul);
+		if(flag != 2)
+			printf(ENTRADA_INVALIDA);
+	} while (flag != 2);
+
+	novo.placar_azul = placarAzul;
+	
+	flag = 0;
+
+	do {
+		scanf("%2[\n]", placarVermelha);
+		getchar();	//Tira \n do buffer
+		flag = strlen(placarVermelha);
+		if(flag != 2)
+			printf(ENTRADA_INVALIDA);
+	} while (flag != 2);
+
+	novo.placar_vermelha = placarVermelha;
+
+	//Leitura do apelido
+
+	scanf("%39[\n]", apelidoMvp);
+	getchar();	//Tira \n do buffer
+	
+	novo.apelido_mvp = apelidoMvp;
+	
+	//Gerar a chave primaria
+
+	/*
+		composicao de letras maiusculas da primeira letra do nome da equipe azul, seguida da primeira letra do nome da equipe vermelha, seguida das duas primeiras letras do apelido do mvp, o dia e o mes da partida.
+
+		Exemplo: FTFE0705
+			Fnatic
+			Team Mid-Solo
+			Ferbien
+			07/05
+	*/
+
+	chavePK[0] = toupper(equipeAzul[0]);
+	chavePK[1] = toupper(equipeVermelha[0]);
+	chavePK[2] = toupper(apelidoMvp[0]);
+	chavePK[3] = toupper(apelidoMvp[2]);
+	
+	strcat(chavePK, dataPartida.dia);
+	strcat(chavePK, dataPartida.mes);
+	chavePK[8] = '\0';
+	
+	// Retorno da struct construida
+
+	return novo;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*======================================================- LIMBO -====================================================== */
+
 
 /* Lê nome da equipe azul */
 char *le_nome_equipe() {
@@ -211,5 +394,15 @@ char *le_apelido_mvp() {
 
 /* Gera a chave primária */
 char *gera_chave_primaria() {
+
+	/*
+		composicao de letras maiusculas da primeira letra do nome da equipe azul, seguida da primeira letra do nome da equipe vermelha, seguida das duas primeiras letras do apelido do mvp, o dia e o mes da partida.
+
+		Exemplo: FTFE0705
+			Fnatic
+			Team Mid-Solo
+			Ferbien
+			07/05
+	*/
 	
 }
